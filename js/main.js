e7819dbea098cd256d3fe08058c5c714442910c6
L-enze <143789408+L-enze@users.noreply.github.com>
2024-01-26 10:32:08 +0100

    p03 les 1

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// function createCard(tittle, description){
+//     let cardhtml = `
+//     <div class="card">
+//         <h2>$(tittle)JAVASCRIPT CARD</h2>
+//         <p>$(discription)PARAGRAAF</p>
+//         <button>klik hier</button>
+//     </div>
+//     `;
+//     return cardhtml;
+// }
+
+// const cardContainer = document.querySelector(".cards-container")
+// for (let i = 0; i <CanvasRenderingContext2D.length)
+// cardContainer.innerhtml += createCard(car.brand, car.description)
+
+const projects = [
+    {title: "hoger lager", 
+    description: "dit was mijn eerste school project", 
+    img: "/img/lego_bruce_wayne.jpg"},
+    ]
+    
+    function createProject(tittle, description, img) {
+    let projectCard = `<h3>prject</h3>
+    <p>descr</p>
+    <img src="/img/lego_bruce_wayne.jpg" alt="">
+    `
+    }
+    
+    const projectContainer = document()
